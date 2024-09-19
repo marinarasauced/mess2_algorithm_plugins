dea3bae845dbd99f80ddabb9bd61b0b7e8b3290f
Marina Nelson <marinarasauced@outlook.com>
2024-09-19 14:44:37 -0400

    directory restructure group cost with actor since cost dependent on actor; cost per edge precalculated as actor attribute ? (along with occupancies which needs to be reimplimented) -> only works well with static environments, maybe make cost func in terms of occupancies like before so it is time expandable

---
+++ src/actor/actor.cpp
@@ -0,0 +1,8 @@
+
+#include "mess2_algorithm_plugins/actor/actor.hpp"
+
+namespace mess2_algorithms
+{
+    Actor::Actor() {};
+
+} // namespace mess2_algorithms
---
+++ src/actor/cost.cpp
@@ -0,0 +1,23 @@
+
+#include "mess2_algorithm_plugins/actor/cost.hpp"
+
+namespace mess2_algorithms
+{
+    // std::vector<double> generate_costs(const Graph& graph)
+    // {
+    //     const auto edges = graph.get_edges_();
+    //     const auto vertices = graph.get_vertices_();
+    //     for (int64_t iter = 0; iter < static_cast<int64_t>(edges.size()); ++iter)
+    //     {
+    //         const auto edge = edges[iter];
+    //         const auto vertex_parent = vertices[edge.get_index_parent_()];
+    //         const auto vertex_child = vertices[edge.get_index_child_()];
+
+    //         const auto type = edge.get_type_();
+    //         if (type=="wait") {
+                
+    //         }
+    //     }
+    // }
+
+} // namespace mess2_algorithms
